ab7f50dc3983fdbd3c5ea61f69f7af11926c0120
Santiago <santiago800g@gmail.com>
2025-11-27 20:41:02 -0600

    Define operation functions

---
+++ code.js
@@ -0,0 +1,19 @@
+function add(numberOne, numberTwo){
+    return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo){
+    return numberOne - numberTwo;
+}
+
+function multiply(numberOne, numberTwo){
+    return numberOne * numberTwo;
+}
+
+function divide(numberOne, numberTwo){
+    return numberOne / numberTwo;
+}
+
+function percentage(numberOne, numberTwo){
+    return numberOne / 100 * numberTwo;
+}
